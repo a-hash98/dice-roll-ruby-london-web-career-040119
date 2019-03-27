236be6713c99f8121c8ac5701bb059be556ea5e8
Ay Has <ahashim983@yahoo.com>
2019-03-27 18:11:10 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,14 +1,17 @@
 # Create method `roll` that returns a random number between 1 and 6
 
 # Feel free to google "how to generate a random number in ruby"
-
-##def roll
-##  return rand(1..6)
-##end
-
 dice = [1,2,3,4,5,6]
-#alternative code
+
 def roll
-  return dice.sample
+  return rand(1..6)
 end
-  # code goes hereend
+
+
+#alternative code?
+
+#def roll
+#  return dice.sample
+#end
+
+# code goes hereend
